3a6147e80500350cab337a00676d2b61412ffce7
Douglas Jasper <doug.jasper@trios.com>
2025-11-04 08:45:18 -0500

    Initial Commit

---
+++ VideoPlayerApp/VideoPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerAppApp.swift
+//  VideoPlayerApp
+//
+//  Created by Douglas Jasper on 2025-11-04.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoPlayerAppTests/VideoPlayerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerAppTests.swift
+//  VideoPlayerAppTests
+//
+//  Created by Douglas Jasper on 2025-11-04.
+//
+
+import Testing
+@testable import VideoPlayerApp
+
+struct VideoPlayerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
