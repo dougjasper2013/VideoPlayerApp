97a5cc250e41fdb092888d6e444554572b700745
Douglas Jasper <doug.jasper@trios.com>
2026-07-06 08:24:27 -0400

    Added Media Folder and Video.swift

---
+++ VideoPlayerApp/Video.swift
@@ -0,0 +1,15 @@
+//
+//  Video.swift
+//  VideoPlayerApp
+//
+//  Created by Douglas Jasper on 2026-07-06.
+//
+
+
+import Foundation
+
+struct Video: Identifiable {
+    let id = UUID()
+    let name: String
+    let fileName: String
+}
