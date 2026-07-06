e2d272fb7daa8b9a7cdd6a7da58adf30fd84af7c
Douglas Jasper <doug.jasper@trios.com>
2026-07-06 10:14:41 -0400

    Updated Video.swift Model

--- MediaPlayerApp/Video.swift
+++ MediaPlayerApp/Video.swift
@@ -7,9 +7,11 @@
 
 
 import Foundation
+import UIKit
 
 struct Video: Identifiable {
     let id = UUID()
     let name: String
     let fileName: String
-}
+    let thumbnail: UIImage?
+    let duration: String}
