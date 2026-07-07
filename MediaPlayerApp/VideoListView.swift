9fca46ff569d0d44769b5f12b926c360450d9913
Douglas Jasper <doug.jasper@trios.com>
2026-07-07 08:14:40 -0400

    Added Thumbnail and Duration to VideoListView

--- MediaPlayerApp/VideoListView.swift
+++ MediaPlayerApp/VideoListView.swift
@@ -14,7 +14,36 @@ struct VideoListView: View {
         NavigationView {
             List(videoLoader.videos) { video in
                 NavigationLink(destination: VideoPlayerView(videoName: video.name)) {
-                    Text(video.name)
+                    
+                    HStack {
+                        // Thumbnail
+                        if let thumbnail = video.thumbnail {
+                            Image(uiImage: thumbnail)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 45)
+                            .clipped()
+                            .cornerRadius(8)
+                        }
+                        else
+                        {
+                            Rectangle()
+                            .fill(Color.gray)
+                            .frame(width: 80, height: 45)
+                            .cornerRadius(8)
+                        }
+
+                        // Video title and duration
+                        VStack(alignment: .leading)
+                        {
+                            Text(video.name)
+                            .font(.headline)
+                            Text(video.duration)
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                        }
+                    }
+                    .padding(.vertical, 5)
                 }
             }
             .navigationTitle("Videos")
